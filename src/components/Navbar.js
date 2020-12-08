739e80ed15cf829dff56601c269ccdf0bcc64117
Master1wistCode <58696699+Master1wistCode@users.noreply.github.com>
2020-12-08 22:24:57 +0600

    node-sass deleted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./../styles/Navbar.scss";
+import "./../styles/Navbar.css";
 import logo from "./../assets/WhiteLogo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
