e952271bb1d892286e03cb8cea2b9b22beb54126
Master1wistCode <58696699+Master1wistCode@users.noreply.github.com>
2021-01-03 19:18:51 +0600

    Fixin' Everything over

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      
     </div>
   );
 }
